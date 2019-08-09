011088b05696981c1b90a368dcb96aab7a333136
bimanneha <biman.neha@gmail.com>
2019-08-09 21:21:01 +0200

    Loading of Thumbnail Images | 9th August - 2019

--- Imgur-Gallery/src/app/app.component.ts
+++ Imgur-Gallery/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountDataService} from "../service/account-data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Imgur-Gallery Sample Application';
+
+  constructor() {
+
+  }
+
 }
--- Imgur-Gallery/src/app/app.module.ts
+++ Imgur-Gallery/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
