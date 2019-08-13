5e3d81ae6d133b09fedc8539b6b9dad757071832
bimanneha <biman.neha@gmail.com>
2019-08-13 11:51:24 +0200

    BEM Methodology Styling | 13th August 2019

--- Imgur-Gallery/src/app/app-routing.module.ts
+++ Imgur-Gallery/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ImageDetailsComponent} from "./image-details/image-details.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  { path:  '', redirectTo:  '', pathMatch:  'full' },
+  { path:  '', redirectTo:  'home', pathMatch:  'full' },
   { path: 'home', component: HomeComponent},
   {path: 'imageDetails/:imageId', component: ImageDetailsComponent},
   { path: '**', component: PageNotFoundComponent }
