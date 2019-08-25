6a4183e2d85f77c6f556a4697ef68cc192a46076
bimanneha <biman.neha@gmail.com>
2019-08-25 02:19:43 +0200

    proxy settings

--- Imgur-Gallery/src/service/imgur-data.service.ts
+++ Imgur-Gallery/src/service/imgur-data.service.ts
@@ -1,11 +1,6 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {UtilityService} from "../utils/utility-service";
-
-const apiPrefix = 'https://api.imgur.com/';
-const galleryPath = '3/gallery/';
-
-const getImages = apiPrefix + galleryPath;
 
 @Injectable({
   providedIn: 'root'
@@ -16,32 +11,22 @@ export class ImgurDataService {
   constructor(private http: HttpClient) {
   }
 
-  static setClientIdHeader(headers: HttpHeaders) {
-    return headers.append('Authorization', 'Client-ID ' + 'b2dd06590440a49');
-  }
-
   apiGetAllImages(newFilterParamObject) {
-    let headers = new HttpHeaders();
-    headers = ImgurDataService.setClientIdHeader(headers);
 
     const filterString = UtilityService.concatURL(newFilterParamObject);
 
-    return this.http.get(getImages + filterString, {headers});
+    return this.http.get('api/' + filterString);
   }
 
   getAllFilteredImages(newFilterParamObject) {
-    let headers = new HttpHeaders();
-    headers = ImgurDataService.setClientIdHeader(headers);
 
     const filterString = UtilityService.concatURL(newFilterParamObject);
 
-    return this.http.get(getImages + filterString, {headers});
+    return this.http.get('api/' + filterString);
   }
 
   getImageDetails(imageId) {
-    let headers = new HttpHeaders();
-    headers = ImgurDataService.setClientIdHeader(headers);
 
-    return this.http.get(getImages + imageId, {headers});
+    return this.http.get('api/' + imageId);
   }
 }
